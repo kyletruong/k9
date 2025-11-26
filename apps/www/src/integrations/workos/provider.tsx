5e1c90ddd9e868d17eba0532b5365a7a10be3cb5
Kyle Truong <kyletruong@gmail.com>
2025-11-25 22:47:05 -0800

    improvement: better brand colors (#37)

--- apps/www/src/integrations/workos/provider.tsx
+++ apps/www/src/integrations/workos/provider.tsx
@@ -3,12 +3,12 @@ import { AuthKitProvider } from '@workos-inc/authkit-react'
 
 const VITE_WORKOS_CLIENT_ID = import.meta.env.VITE_WORKOS_CLIENT_ID
 if (!VITE_WORKOS_CLIENT_ID) {
-  throw new Error('Add your WorkOS Client ID to the .env.local file')
+  throw new Error('Missing WORKOS_CLIENT_ID')
 }
 
 const VITE_WORKOS_API_HOSTNAME = import.meta.env.VITE_WORKOS_API_HOSTNAME
 if (!VITE_WORKOS_API_HOSTNAME) {
-  throw new Error('Add your WorkOS API Hostname to the .env.local file')
+  throw new Error('Missing WORKOS_API_HOSTNAME')
 }
 
 export default function AppWorkOSProvider({
