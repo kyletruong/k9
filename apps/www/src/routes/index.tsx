0d25e2a82ed8487a13c8887dbcbf5e54ca05513f
kyletruong <kyletruong@gmail.com>
2025-11-26 00:45:27 -0800

    improvement: Use the golden ratio for top padding

--- apps/www/src/routes/index.tsx
+++ apps/www/src/routes/index.tsx
@@ -5,7 +5,7 @@ export const Route = createFileRoute('/')({ component: App })
 function App() {
   return (
     <div className='min-h-screen'>
-      <div className='flex flex-col items-center pt-[40vh]'>
+      <div className='flex flex-col items-center pt-[38vh]'>
         <p className='text-7xl font-bold'>[WIP]</p>
       </div>
     </div>
