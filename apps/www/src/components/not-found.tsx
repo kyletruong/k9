590a5d5ef010189923e075d2da238998ac9f1d8f
kyletruong <kyletruong@gmail.com>
2025-11-26 00:54:45 -0800

    feat: Update 404 button wording

--- apps/www/src/components/not-found.tsx
+++ apps/www/src/components/not-found.tsx
@@ -6,7 +6,7 @@ export function NotFound() {
     <div className='flex flex-col min-h-screen items-center pt-[38vh]'>
       <h1 className='mb-6 text-8xl font-semibold'>404</h1>
       <Button asChild className='text-base' size='lg'>
-        <Link to='/'>Back to home page</Link>
+        <Link to='/'>Take me home</Link>
       </Button>
     </div>
   )
