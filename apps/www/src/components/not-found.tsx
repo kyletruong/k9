9f8776e7c3ecdf5acad4632a55ededbecada3cf1
Kyle Truong <kyletruong@gmail.com>
2025-11-26 00:45:46 -0800

    improvement: Use the golden ratio for top padding (#44)

--- apps/www/src/components/not-found.tsx
+++ apps/www/src/components/not-found.tsx
@@ -3,7 +3,7 @@ import { Button } from './ui/button'
 
 export function NotFound() {
   return (
-    <div className='flex flex-col min-h-screen items-center justify-center'>
+    <div className='flex flex-col min-h-screen items-center pt-[38vh]'>
       <h1 className='mb-6 text-8xl font-semibold'>404</h1>
       <Button className='text-base' size='lg'>
         <Link to='/'>Back to home page</Link>
