fcb95f3d5a314aca2fea14768642d2c59233ee89
kyletruong <kyletruong@gmail.com>
2025-11-26 00:46:53 -0800

    fix: Fix nested interactive element issue

--- apps/www/src/components/not-found.tsx
+++ apps/www/src/components/not-found.tsx
@@ -5,7 +5,7 @@ export function NotFound() {
   return (
     <div className='flex flex-col min-h-screen items-center pt-[38vh]'>
       <h1 className='mb-6 text-8xl font-semibold'>404</h1>
-      <Button className='text-base' size='lg'>
+      <Button asChild className='text-base' size='lg'>
         <Link to='/'>Back to home page</Link>
       </Button>
     </div>
