762e95cb0942e14fe2e5a2fa6362f85eb1627d50
Kyle Truong <kyletruong@gmail.com>
2025-12-18 10:27:40 -0800

    refactor: Update take me home button on not-found

--- apps/www/src/components/not-found.tsx
+++ apps/www/src/components/not-found.tsx
@@ -1,14 +1,12 @@
 import { buttonVariants } from '@repo/ui/components/button'
+import { cn } from '@repo/ui/lib/utils'
 import { Link } from '@tanstack/react-router'
 
 export function NotFound() {
   return (
     <div className='flex flex-col min-h-screen items-center pt-[38vh]'>
       <h1 className='mb-6 text-8xl font-semibold'>404</h1>
-      <Link
-        className={buttonVariants({ className: 'text-base', size: 'lg' })}
-        to='/'
-      >
+      <Link className={cn(buttonVariants({ size: 'lg' }), 'text-xl')} to='/'>
         Take me home
       </Link>
     </div>
