502541e739aeebcf4d94f6cd9a8a88bb718ec072
Kyle Truong <kyletruong@gmail.com>
2025-12-17 22:20:49 -0800

    feat: Migrate to base-ui and update theme to lime

--- apps/www/src/components/not-found.tsx
+++ apps/www/src/components/not-found.tsx
@@ -1,13 +1,16 @@
-import { Button } from '@repo/ui/components/button'
+import { buttonVariants } from '@repo/ui/components/button'
 import { Link } from '@tanstack/react-router'
 
 export function NotFound() {
   return (
     <div className='flex flex-col min-h-screen items-center pt-[38vh]'>
       <h1 className='mb-6 text-8xl font-semibold'>404</h1>
-      <Button asChild className='text-base' size='lg'>
-        <Link to='/'>Take me home</Link>
-      </Button>
+      <Link
+        className={buttonVariants({ className: 'text-base', size: 'lg' })}
+        to='/'
+      >
+        Take me home
+      </Link>
     </div>
   )
 }
